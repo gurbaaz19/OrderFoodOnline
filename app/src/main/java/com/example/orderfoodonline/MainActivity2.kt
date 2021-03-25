92a4d63e3165d930abd1957b14c59ffcd08d0c0b
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-03-25 17:12:39 +0530

    2nd Commit

--- app/src/main/java/com/example/orderfoodonline/MainActivity2.kt
+++ app/src/main/java/com/example/orderfoodonline/MainActivity2.kt
@@ -1,12 +1,18 @@
 package com.example.orderfoodonline
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.internal.NavigationMenuItemView
 
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
@@ -14,12 +20,12 @@ class MainActivity2 : AppCompatActivity() {
         val apFragment = ApFragment()
         val mcFragment = McFragment()
         val dsFragment = DsFragment()
-        val bottomNavigationView= findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
         setCurrentFragment(apFragment)
 
         bottomNavigationView.setOnNavigationItemSelectedListener {
-            when(it.itemId){
+            when (it.itemId) {
                 R.id.miap -> setCurrentFragment(apFragment)
                 R.id.mimc -> setCurrentFragment(mcFragment)
                 R.id.mids -> setCurrentFragment(dsFragment)
@@ -27,10 +33,24 @@ class MainActivity2 : AppCompatActivity() {
             true
         }
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        val apFragment = ApFragment()
+//        val mcFragment = McFragment()
+//        val dsFragment = DsFragment()
+//
+//        when(item.itemId){
+//            R.id.miorder -> setCurrentFragment()
+//        }
+//        return true
+//    }
 
-    private fun setCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, fragment)
-            commit()
-        }
-}
+        private fun setCurrentFragment(fragment: Fragment) =
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, fragment)
+                commit()
+            }
+    }
